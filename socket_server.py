00becef44e01fbdcc3cfece0b615f1f3f3ab21c8
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2024-08-25 19:29:09 -0300

    feat(socket_server.py): add finish file send

--- socket_server.py
+++ socket_server.py
@@ -5,6 +5,8 @@ import os
 from websockets.asyncio.server import serve
 import msgpack
 
+file_path = ""
+
 def read_chunked_binary(file_path, chunk_size=1024):
     if os.path.exists(file_path):
         with open(file_path, 'rb') as file:
@@ -15,20 +17,30 @@ def read_chunked_binary(file_path, chunk_size=1024):
                 yield chunk
 
 async def send_file(websocket, path, chunk_size=1024):
+    try:
         for chunk in read_chunked_binary(path, chunk_size):
             packed_chunk = msgpack.packb(chunk)
             await websocket.send(packed_chunk)
-    
+        
+        await websocket.send("finished")
+        print("File sent successfully")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+    finally:
+        await websocket.close() 
+
 async def hello(websocket):
     name = await websocket.recv()
-    print(f"<<< {name}")
-
-    file_path = "meme.jpg"
-    await send_file(websocket, file_path)
+    if name == "finished":
+        print("Finished")
+        pass
+    if name == "file":
+        await send_file(websocket, file_path)
 
 async def main():
     async with serve(hello, "0.0.0.0", 8030):
         await asyncio.get_running_loop().create_future()  # run forever
 
 if __name__ == "__main__":
+    file_path = input("Enter the file path: ")
     asyncio.run(main())
